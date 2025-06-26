1365bf36d08327c192e294cfccf8a887c4cc66ef
Alphonse <kubalpha19@gmail.com>
2025-06-26 00:43:08 -0700

    Apdated files

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,44 +1,71 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import OwlCarousel from 'react-owl-carousel';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 const HotCollections = () => {
+  const [userData,setUserData]=useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections");
+        setUserData(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    
+   fetchData();
+  }, []);
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
-            <div className="text-center">
+            <div className="text-center">              
               <h2>Hot Collections</h2>
-              <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+            <OwlCarousel className=""  key={userData.length}  loop margin={10} nav items={4}>
+          { userData.map((HotColl) => (
+             <div key={HotColl.id} id={HotColl.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                  <Link to="/item-details" state={HotColl.nftId
+}>
+
+                    <img src={HotColl.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={HotColl.authorImage}alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{HotColl.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC-{HotColl.code}</span>
                 </div>
+                
               </div>
+              
             </div>
+            
           ))}
+          </OwlCarousel>
+    
+
         </div>
-      </div>
+      </div>   
+      
     </section>
   );
 };
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,63 +1,80 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
-
+import axios from "axios";
 const ItemDetails = () => {
+  const location = useLocation();
+  const nftId = location.state; // Getting the passed ID from Link
+   console.log(nftId)
+  const [nftData,setNftData] = useState(); // Will hold the matching NFT item
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+        );
+        console.log(data)
+        const matchedItem = data.find((item) =>item.nftId===nftId);
+        console.log(matchedItem)
+        setNftData(matchedItem);
+        window.scrollTo(0, 0);
+      } catch (error) {
+        console.error("Error fetching NFT item:", error);
+      }
+    };
+    fetchData();
+  }, [nftId]);
+    console.log(nftData)
+  // While loading or not found
+  if (!nftData) return <div>Loading item details...</div>;
+   console.log(nftData)
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
-        <div id="top"></div>
         <section aria-label="section" className="mt90 sm-mt-0">
           <div className="container">
             <div className="row">
               <div className="col-md-6 text-center">
                 <img
-                  src={nftImage}
+                  src={nftData.nftImage}
                   className="img-fluid img-rounded mb-sm-30 nft-image"
                   alt=""
                 />
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  <h2>{nftData.title}</h2>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
-                      <i className="fa fa-eye"></i>
-                      100
+                      <i className="fa fa-eye"></i> 100
                     </div>
                     <div className="item_info_like">
-                      <i className="fa fa-heart"></i>
-                      74
+                      <i className="fa fa-heart"></i> {nftData.likes}
                     </div>
                   </div>
-                  <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
-                  </p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eius quam error omnis quibusdam excepturi provident at libero nulla incidunt facere, rem ullam ipsam praesentium ducimus nisi ad perspiciatis id.
+
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
                           <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                            <img
+                              className="lazy"
+                              src={nftData.authorImage}
+                              alt=""
+                            />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to="/author">{nftData.owner}</Link>
                         </div>
                       </div>
                     </div>
-                    <div></div>
                   </div>
                   <div className="de_tab tab_simple">
                     <div className="de_tab_content">
@@ -65,26 +82,31 @@ const ItemDetails = () => {
                       <div className="item_author">
                         <div className="author_list_pp">
                           <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                            <img
+                              className="lazy"
+                              src={AuthorImage}
+                              alt=""
+                            />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to="/author">{nftData.title || "Unknown"}</Link>
                         </div>
                       </div>
                     </div>
+
                     <div className="spacer-40"></div>
                     <h6>Price</h6>
                     <div className="nft-item-price">
                       <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      <span>{nftData.price} ETH</span>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
+              </div> {/* end .col-md-6 */}
+            </div> {/* end .row */}
+          </div> {/* end .container */}
         </section>
       </div>
     </div>
